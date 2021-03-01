23b3384bf9296f185dbe9fa8c4d0bf4f49c9ad3b
joankamere <joankamere@gmail.com>
2021-03-01 16:00:20 +0300

    save changes to design

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+$(document).ready(function(){
     $("#work1").mouseover(function(){
       $("#hover1").show();
     }).mouseout(function(){
