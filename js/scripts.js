d19276946b8f31a05cd55264ff93caa068915af8
joankamere <joankamere@gmail.com>
2021-03-01 16:48:29 +0300

    add descriptions to what we do

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,26 @@ $(document).ready(function(){
       $("#design-image").slideDown('1500');
     });
   });
+  $(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
+    });
+  });
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#pm").show('1500');
+    });
+    $("#pm").click(function(){
+      $("#pm").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
 $(document).ready(function(){
     $("#work1").mouseover(function(){
       $("#hover1").show();
