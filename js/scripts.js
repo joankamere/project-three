709d0f805a7e2591ee9f17561fb827661c76115a
joankamere <joankamere@gmail.com>
2021-03-03 17:53:38 +0300

    include hover

--- js/scripts.js
+++ js/scripts.js
@@ -17,62 +17,15 @@ $(document).ready(function() {
       $("#pm").toggle();
     });
   });
-$(document).ready(function(){
-  $("#work1").mouseover(function(){
-    $("#hover1").show();
-  }).mouseout(function(){
-    $("#hover1").hide();
+  $(document).ready(function () {
+    $(".mouse").hover(function () {
+      $(this).animate({ opacity: '1' });
+    },
+      function () {
+        $(this).animate({ opacity: '0' });
+      })
   });
-});
-$(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#hover2").show();
-    }).mouseout(function(){
-      $("#hover2").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#hover3").show();
-    }).mouseout(function(){
-      $("#hover3").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#hover4").show();
-    }).mouseout(function(){
-      $("#hover4").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#hover5").show();
-    }).mouseout(function(){
-      $("#hover5").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work6").mouseover(function(){
-      $("#hover6").show();
-    }).mouseout(function(){
-      $("#hover6").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work7").mouseover(function(){
-      $("#hover7").show();
-    }).mouseout(function(){
-      $("#hover7").hide();
-    });
-});
-$(document).ready(function(){
-    $("#work8").mouseover(function(){
-      $("#hover8").show();
-    }).mouseout(function(){
-      $("#hover8").hide();
-    });
-});
+
 $(document).ready(function(){
   $("form#formXYZ").submit(function(event){
     var name = $("input#ALT1").val();
